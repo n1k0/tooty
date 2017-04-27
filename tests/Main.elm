4fc5cd0135f78b13498b4fdea66a9b19a1cc7451
Nicolas Perriault <nicolas@perriault.net>
2017-04-27 16:21:19 +0200

    Updated test fs organization.

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,13 +1,13 @@
 port module Main exposing (..)
 
-import NotificationTests
+import MastodonTest.HelperTest
 import Test.Runner.Node exposing (run, TestProgram)
 import Json.Encode exposing (Value)
 
 
 main : TestProgram
 main =
-    run emit NotificationTests.all
+    run emit MastodonTest.HelperTest.all
 
 
 port emit : ( String, Value ) -> Cmd msg
