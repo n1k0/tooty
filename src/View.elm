b7304a58c3f0587e8c08d986ba92742fc05617b8
Nicolas Perriault <nicolas@perriault.net>
2017-05-01 23:22:19 +0200

    Part 3: The sun is rising.
    The darkness is gently fading over, dawn is near. I can
    now see mountains drawing in the horizon.
    
    Elm is rather quiet, but keeps scrutating the shadows.

--- src/View.elm
+++ src/View.elm
@@ -13,6 +13,7 @@ import Views.Auth exposing (authView)
 import Views.Common as Common
 import Views.Draft exposing (draftView)
 import Views.Error exposing (errorsListView)
+import Views.Notification exposing (notificationListView)
 import Views.Status exposing (statusView, statusActionsView, statusEntryView)
 import Views.Thread exposing (threadView)
 import Views.Viewer exposing (viewerView)
@@ -39,116 +40,6 @@ timelineView ( label, iconName, context, currentUser, statuses ) =
                     [ div [ class "panel-heading" ] [ Common.icon iconName, text label ] ]
                 , Keyed.ul [ id context, class "list-group timeline" ] <|
                     List.map keyedEntry statuses
-                ]
-            ]
-
-
-notificationHeading : List Account -> String -> String -> Html Msg
-notificationHeading accounts str iconType =
-    div [ class "status-info" ]
-        [ div [ class "avatars" ] <| List.map Common.accountAvatarLink accounts
-        , p [ class "status-info-text" ] <|
-            List.intersperse (text " ")
-                [ Common.icon iconType
-                , span [] <| List.intersperse (text ", ") (List.map Common.accountLink accounts)
-                , text str
-                ]
-        ]
-
-
-notificationStatusView : ( String, CurrentUser, Status, NotificationAggregate ) -> Html Msg
-notificationStatusView ( context, currentUser, status, { type_, accounts } ) =
-    div [ class <| "notification " ++ type_ ]
-        [ case type_ of
-            "reblog" ->
-                notificationHeading accounts "boosted your toot" "fire"
-
-            "favourite" ->
-                notificationHeading accounts "favourited your toot" "star"
-
-            _ ->
-                text ""
-        , lazy2 statusView context status
-        , lazy2 statusActionsView status currentUser
-        ]
-
-
-notificationFollowView : CurrentUser -> NotificationAggregate -> Html Msg
-notificationFollowView currentUser { accounts } =
-    let
-        profileView account =
-            div [ class "status follow-profile" ]
-                [ Common.accountAvatarLink account
-                , div [ class "username" ] [ Common.accountLink account ]
-                , p
-                    [ class "status-text"
-                    , onClick <| LoadAccount account.id
-                    ]
-                  <|
-                    formatContent account.note []
-                ]
-    in
-        div [ class "notification follow" ]
-            [ notificationHeading accounts "started following you" "user"
-            , div [ class "" ] <| List.map profileView accounts
-            ]
-
-
-notificationEntryView : CurrentUser -> NotificationAggregate -> Html Msg
-notificationEntryView currentUser notification =
-    li [ class "list-group-item" ]
-        [ case notification.status of
-            Just status ->
-                lazy notificationStatusView ( "notification", currentUser, status, notification )
-
-            Nothing ->
-                notificationFollowView currentUser notification
-        ]
-
-
-notificationFilterView : NotificationFilter -> Html Msg
-notificationFilterView filter =
-    let
-        filterBtn tooltip iconName event =
-            button
-                [ class <|
-                    if filter == event then
-                        "btn btn-primary"
-                    else
-                        "btn btn-default"
-                , title tooltip
-                , onClick <| FilterNotifications event
-                ]
-                [ Common.icon iconName ]
-    in
-        Common.justifiedButtonGroup
-            [ filterBtn "All notifications" "asterisk" NotificationAll
-            , filterBtn "Mentions" "share-alt" NotificationOnlyMentions
-            , filterBtn "Boosts" "fire" NotificationOnlyBoosts
-            , filterBtn "Favorites" "star" NotificationOnlyFavourites
-            , filterBtn "Follows" "user" NotificationOnlyFollows
-            ]
-
-
-notificationListView : CurrentUser -> NotificationFilter -> List NotificationAggregate -> Html Msg
-notificationListView currentUser filter notifications =
-    let
-        keyedEntry notification =
-            ( toString notification.id
-            , lazy2 notificationEntryView currentUser notification
-            )
-    in
-        div [ class "col-md-3 column" ]
-            [ div [ class "panel panel-default notifications-panel" ]
-                [ a
-                    [ href "", onClickWithPreventAndStop <| ScrollColumn ScrollTop "notifications" ]
-                    [ div [ class "panel-heading" ] [ Common.icon "bell", text "Notifications" ] ]
-                , notificationFilterView filter
-                , Keyed.ul [ id "notifications", class "list-group timeline" ] <|
-                    (notifications
-                        |> filterNotifications filter
-                        |> List.map keyedEntry
-                    )
                 ]
             ]
 
---
+++ src/Views/Notification.elm
@@ -0,0 +1,126 @@
+module Views.Notification exposing (notificationListView)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Keyed as Keyed
+import Html.Lazy as Lazy
+import Mastodon.Model exposing (..)
+import Types exposing (..)
+import Views.Common as Common
+import ViewHelper exposing (..)
+import Views.Status exposing (statusActionsView, statusView)
+
+
+type alias CurrentUser =
+    Account
+
+
+notificationHeading : List Account -> String -> String -> Html Msg
+notificationHeading accounts str iconType =
+    div [ class "status-info" ]
+        [ div [ class "avatars" ] <| List.map Common.accountAvatarLink accounts
+        , p [ class "status-info-text" ] <|
+            List.intersperse (text " ")
+                [ Common.icon iconType
+                , span [] <| List.intersperse (text ", ") (List.map Common.accountLink accounts)
+                , text str
+                ]
+        ]
+
+
+notificationStatusView : ( String, CurrentUser, Status, NotificationAggregate ) -> Html Msg
+notificationStatusView ( context, currentUser, status, { type_, accounts } ) =
+    div [ class <| "notification " ++ type_ ]
+        [ case type_ of
+            "reblog" ->
+                notificationHeading accounts "boosted your toot" "fire"
+
+            "favourite" ->
+                notificationHeading accounts "favourited your toot" "star"
+
+            _ ->
+                text ""
+        , Lazy.lazy2 statusView context status
+        , Lazy.lazy2 statusActionsView status currentUser
+        ]
+
+
+notificationFollowView : CurrentUser -> NotificationAggregate -> Html Msg
+notificationFollowView currentUser { accounts } =
+    let
+        profileView account =
+            div [ class "status follow-profile" ]
+                [ Common.accountAvatarLink account
+                , div [ class "username" ] [ Common.accountLink account ]
+                , p
+                    [ class "status-text"
+                    , onClick <| LoadAccount account.id
+                    ]
+                  <|
+                    formatContent account.note []
+                ]
+    in
+        div [ class "notification follow" ]
+            [ notificationHeading accounts "started following you" "user"
+            , div [ class "" ] <| List.map profileView accounts
+            ]
+
+
+notificationEntryView : CurrentUser -> NotificationAggregate -> Html Msg
+notificationEntryView currentUser notification =
+    li [ class "list-group-item" ]
+        [ case notification.status of
+            Just status ->
+                Lazy.lazy notificationStatusView ( "notification", currentUser, status, notification )
+
+            Nothing ->
+                notificationFollowView currentUser notification
+        ]
+
+
+notificationFilterView : NotificationFilter -> Html Msg
+notificationFilterView filter =
+    let
+        filterBtn tooltip iconName event =
+            button
+                [ class <|
+                    if filter == event then
+                        "btn btn-primary"
+                    else
+                        "btn btn-default"
+                , title tooltip
+                , onClick <| FilterNotifications event
+                ]
+                [ Common.icon iconName ]
+    in
+        Common.justifiedButtonGroup
+            [ filterBtn "All notifications" "asterisk" NotificationAll
+            , filterBtn "Mentions" "share-alt" NotificationOnlyMentions
+            , filterBtn "Boosts" "fire" NotificationOnlyBoosts
+            , filterBtn "Favorites" "star" NotificationOnlyFavourites
+            , filterBtn "Follows" "user" NotificationOnlyFollows
+            ]
+
+
+notificationListView : CurrentUser -> NotificationFilter -> List NotificationAggregate -> Html Msg
+notificationListView currentUser filter notifications =
+    let
+        keyedEntry notification =
+            ( toString notification.id
+            , Lazy.lazy2 notificationEntryView currentUser notification
+            )
+    in
+        div [ class "col-md-3 column" ]
+            [ div [ class "panel panel-default notifications-panel" ]
+                [ a
+                    [ href "", onClickWithPreventAndStop <| ScrollColumn ScrollTop "notifications" ]
+                    [ div [ class "panel-heading" ] [ Common.icon "bell", text "Notifications" ] ]
+                , notificationFilterView filter
+                , Keyed.ul [ id "notifications", class "list-group timeline" ] <|
+                    (notifications
+                        |> filterNotifications filter
+                        |> List.map keyedEntry
+                    )
+                ]
+            ]
