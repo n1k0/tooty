a11fc8e346191f6754592aaa624eb3c350b323d6
Nicolas Lœuillet <nicolas@loeuillet.org>
2017-05-03 10:16:31 +0200

    Added link to external profile in account view

--- src/View/Account.elm
+++ src/View/Account.elm
@@ -152,7 +152,9 @@ accountView currentUser account relationship panelContent =
                         ]
                         [ div [ class "opacity-layer" ]
                             [ followButton currentUser relationship account
-                            , img [ src account.avatar ] []
+                            , a
+                                [ href account.url ]
+                                [ img [ src account.avatar ] [] ]
                             , span [ class "account-display-name" ] [ text account.display_name ]
                             , span [ class "account-username" ] [ text ("@" ++ account.username) ]
                             , span [ class "account-note" ] (formatContent account.note [])
