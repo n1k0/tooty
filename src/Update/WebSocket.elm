5c863f916deb6155ca8da232e7330dbb8f565d69
Nicolas Perriault <nicolas@perriault.net>
2017-05-08 12:45:05 +0200

    Update thread and account views with new related statuses.

--- src/Update/WebSocket.elm
+++ src/Update/WebSocket.elm
@@ -2,10 +2,56 @@ module Update.WebSocket exposing (update)
 
 import Mastodon.Decoder
 import Mastodon.Helper
+import Mastodon.Model exposing (..)
 import Mastodon.WebSocket
 import Types exposing (..)
 import Update.Error exposing (..)
 import Update.Timeline
+
+
+updateCurrentViewWithStatus : Status -> Model -> Model
+updateCurrentViewWithStatus status model =
+    case model.currentView of
+        ThreadView thread ->
+            case status.in_reply_to_id of
+                Nothing ->
+                    model
+
+                Just inReplyToId ->
+                    let
+                        threadStatusIds =
+                            List.concat
+                                [ [ thread.status.id ]
+                                , List.map .id thread.context.ancestors
+                                , List.map .id thread.context.descendants
+                                ]
+
+                        threadMember =
+                            List.member inReplyToId threadStatusIds
+                    in
+                        if threadMember then
+                            let
+                                context =
+                                    thread.context
+
+                                newContext =
+                                    { context | descendants = List.concat [ thread.context.descendants, [ status ] ] }
+
+                                newView =
+                                    ThreadView { thread | context = newContext }
+                            in
+                                { model | currentView = newView }
+                        else
+                            model
+
+        AccountView account ->
+            if Mastodon.Helper.sameAccount account status.account then
+                { model | accountTimeline = Update.Timeline.prepend status model.accountTimeline }
+            else
+                model
+
+        _ ->
+            model
 
 
 update : WebSocketMsg -> Model -> ( Model, Cmd Msg )
@@ -19,7 +65,11 @@ update msg model =
                 Mastodon.WebSocket.StatusUpdateEvent result ->
                     case result of
                         Ok status ->
-                            { model | homeTimeline = Update.Timeline.prepend status model.homeTimeline } ! []
+                            (model
+                                |> (\m -> { m | homeTimeline = Update.Timeline.prepend status m.homeTimeline })
+                                |> updateCurrentViewWithStatus status
+                            )
+                                ! []
 
                         Err error ->
                             { model | errors = addErrorNotification error model } ! []
@@ -60,7 +110,11 @@ update msg model =
                 Mastodon.WebSocket.StatusUpdateEvent result ->
                     case result of
                         Ok status ->
-                            { model | localTimeline = Update.Timeline.prepend status model.localTimeline } ! []
+                            (model
+                                |> (\m -> { m | localTimeline = Update.Timeline.prepend status m.localTimeline })
+                                |> updateCurrentViewWithStatus status
+                            )
+                                ! []
 
                         Err error ->
                             { model | errors = addErrorNotification error model } ! []
@@ -84,7 +138,11 @@ update msg model =
                 Mastodon.WebSocket.StatusUpdateEvent result ->
                     case result of
                         Ok status ->
-                            { model | globalTimeline = Update.Timeline.prepend status model.globalTimeline } ! []
+                            (model
+                                |> (\m -> { m | globalTimeline = Update.Timeline.prepend status m.globalTimeline })
+                                |> updateCurrentViewWithStatus status
+                            )
+                                ! []
 
                         Err error ->
                             { model | errors = addErrorNotification error model } ! []
